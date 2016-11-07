4407fc4d29040fd72ab15ed8070308068944c362
NoFootDancer <mack.arron.0@gmail.com>
2016-11-07 10:08:17 -0800

    Index preparado

--- landings/resources/views/index.php
+++ landings/resources/views/index.php
@@ -1,9 +1,30 @@
 <!DOCTYPE html>
 <html ng-app="landingapp">
 <head>
-	<title>Landing Page Generator	</title>
+	<title>Landing Page Generator</title>
+
+	<link rel="stylesheet" type="text/css" href="bower_components/angular-datatables/dist/css/angular-datables.css">
+	<link rel="stylesheet" type="text/css" href="bower_components/bootstrap/dist/css/bootstrap.css">
+	
 </head>
 <body>
+<div class="container" ng-view></div>
+
+
+<!--Scripts-->
+<script type="text/javascript" src="bower_components/jquery/dist/jquery.js"></script> <!-- Jquery-->
+<script type="text/javascript" src="bower_components/angular/angular.js"></script><!-- Angular -->
+<script type="text/javascript" src="bower_components/angular-datatables/dist/angular-datatables.js"></script> <!-- Angular DT-->
+<script type="text/javascript" src="bower_components/angular-route/angular-route.js"></script> <!--Angular Route-->
+<script type="text/javascript" src="bower_components/bootstrap/dist/js/bootstrap.js"></script> <!--Bootstrap-->
+<script type="text/javascript" src="bower_components/datatables.net/js/jquery.dataTables.js"></script> <!--Jquery DT-->
+
+
+
+
+
+<!-- Modulos -->
+<script type="text/javascript" src="app/app.module.js"></script>
 
 </body>
 </html>
